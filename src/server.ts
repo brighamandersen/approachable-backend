ced8779df9736dc696528fb160db013609237d96
Brigham Andersen <brighambandersen@gmail.com>
2024-02-28 18:18:46 -0500

    Sending flat userlocation list

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,46 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+const PORT = 3000;
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  // email: string;
+  // password: string;
+}
+
+interface UserLocation {
+  userId: number;
+  latitude: number;
+  longitude: number;
+}
 
 app.get('/', async (req: Request, res: Response) => {
-  console.log('Hello world');
   res.send('Hello world');
 });
 
-const port = 8080;
+app.get(
+  '/get-user-locations',
+  async (req: Request, res: Response<UserLocation[]>) => {
+    console.log('Hello world');
+    const userLocations: UserLocation[] = [
+      {
+        userId: 1,
+        latitude: 40.7128,
+        longitude: -74.006
+      },
+      {
+        userId: 2,
+        latitude: 34.0522,
+        longitude: -118.2437
+      }
+    ];
+    res.send(userLocations);
+  }
+);
 
-app.listen(port, (): void => {
-  console.log(`App is listening at http://localhost:${port}`);
+app.listen(PORT, (): void => {
+  console.log(`App is listening at http://localhost:${PORT}`);
 });
-
-console.log('Hello World');
