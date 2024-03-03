35e9f61e909c0f0ffc50341c72eea522a1c70811
Brigham Andersen <brighambandersen@gmail.com>
2024-03-03 14:07:22 -0500

    Remove comments

--- src/fill_tables.sql
+++ src/fill_tables.sql
@@ -1,7 +1,5 @@
--- Insert users into User table
 INSERT INTO User (firstName, lastName) VALUES ('Taylor', 'English');
 INSERT INTO User (firstName, lastName) VALUES ('Brigham', 'Andersen');
 
--- Insert user locations into UserLocation table
 INSERT INTO UserLocation (userId, latitude, longitude) VALUES (1, 40.7128, -74.006);
 INSERT INTO UserLocation (userId, latitude, longitude) VALUES (2, 34.0522, -118.2437);
