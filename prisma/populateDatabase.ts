b45f92b7b195bb49ab507e839b9ae3eb854af331
Brigham Andersen <brighambandersen@gmail.com>
2024-03-21 21:22:06 -0400

    Add taylor's coordinates to populate script

--- prisma/populateDatabase.ts
+++ prisma/populateDatabase.ts
@@ -8,17 +8,6 @@ async function main() {
   try {
     await prisma.user.create({
       data: {
-        firstName: 'Taylor',
-        lastName: 'English',
-        birthDate: JAN_1ST_2000,
-        bio: 'I am fluent in English',
-        latitude: 40.0,
-        longitude: 50.0,
-        locationLastUpdated: 1704067200
-      }
-    });
-    await prisma.user.create({
-      data: {
         firstName: 'Brigham',
         lastName: 'Andersen',
         birthDate: JAN_1ST_2000,
@@ -39,6 +28,40 @@ async function main() {
         locationLastUpdated: 1710031975
       }
     });
+    await prisma.user.create({
+      data: {
+        firstName: 'Taylor',
+        lastName: 'English',
+        birthDate: JAN_1ST_2000,
+        bio: 'I love some good English',
+        latitude: 37.33490417052106,
+        longitude: -122.00879734210524,
+        locationLastUpdated: 1710031975
+      }
+    });
+    await prisma.user.create({
+      data: {
+        firstName: 'Joe',
+        lastName: 'Bob',
+        birthDate: JAN_1ST_2000,
+        bio: 'The name is Joe Bob, not Jim Bob',
+        latitude: 37.33476720715561,
+        longitude: -122.00837388626947,
+        locationLastUpdated: 1710031975
+      }
+    });
+    await prisma.user.create({
+      data: {
+        firstName: 'Suzy',
+        lastName: 'Shmoozy',
+        birthDate: JAN_1ST_2000,
+        bio: 'Catch me shmoozing dinoco',
+        latitude: 37.33432494345617,
+        longitude: -122.00865021585727,
+        locationLastUpdated: 1710031975
+      }
+    });
+
     console.log('Data inserted successfully');
   } catch (error) {
     console.error('Error inserting data:', error);
