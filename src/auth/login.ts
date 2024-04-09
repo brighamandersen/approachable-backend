5cdeac314abf7fec62cd6b64bd6679e721ef30d8
Brigham Andersen <brighambandersen@gmail.com>
2024-04-08 22:23:40 -0400

    Return back user

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -16,11 +16,7 @@ const login = async (
   const { userId } = req.body;
 
   if (!userId) {
-    return res.status(400).send('userId is required');
-  }
-
-  if (req.session?.userId) {
-    res.status(200).send(`User ${req.session.userId} is already logged in`);
+    res.status(400).send('userId is required');
     return;
   }
 
@@ -36,15 +32,13 @@ const login = async (
       res.status(404).send(`User with id ${userId} not found`);
       return;
     }
+
+    req.session.userId = userId;
+    res.status(200).send(user);
   } catch (error) {
     console.error('Error logging in:', error);
     res.status(500).send('Internal Server Error');
-    return;
   }
-
-  req.session.userId = userId;
-
-  res.status(200).send(`User ${userId} logged in successfully`);
 };
 
 export default login;
