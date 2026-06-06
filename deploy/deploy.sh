f6756c0641d5afc8b2c15c282138ac92a4f0d5d6
Brigham Andersen <brighambandersen@gmail.com>
2026-06-05 23:52:44 -0600

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,19 +5,19 @@ echo "Deploying approachable-api"
 
 # nginx
 
-sudo cp /home/brig/code/approachable-backend/deploy/nginx.conf /etc/nginx/conf.d/approachable-api.conf
+sudo cp /home/brig/dev/approachable-backend/deploy/nginx.conf /etc/nginx/conf.d/approachable-api.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
 
 # backend
 
-cd /home/brig/code/approachable-backend
+cd /home/brig/dev/approachable-backend
 npm run build
 
 # systemd
 
-sudo cp /home/brig/code/approachable-backend/deploy/systemd.service /etc/systemd/system/approachable-api.service
+sudo cp /home/brig/dev/approachable-backend/deploy/systemd.service /etc/systemd/system/approachable-api.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable approachable-api.service
